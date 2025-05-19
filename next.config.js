129d551b437d9cc1867d5dbcde674d55da1552ad
HeinHtetSoe <heinhtetsoe1821@gmail.com>
2025-05-19 09:03:23 +0700

    Add GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
     domains: ['github.com'],
   },
   reactStrictMode: true,
+  basePath: '/Portfolio',
+  assetPrefix: '/Portfolio/',
 }
 
 module.exports = nextConfig 
