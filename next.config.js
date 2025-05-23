5499cbdae51563163be8bb5f0155d5e1f395e539
callmeHeinHtet <heinhtetsoe1821@gmail.com>
2025-05-24 02:17:44 +0700

    Add GitHub Actions workflow for deployment

--- next.config.js
+++ next.config.js
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
-const isProduction = process.env.NODE_ENV === 'production';
-const REPO_NAME = 'Portfolio';
-
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  reactStrictMode: true,
-  basePath: isProduction ? `/${REPO_NAME}` : '',
-  assetPrefix: isProduction ? `/${REPO_NAME}/` : '',
+  basePath: '/Portfolio',
+  assetPrefix: '/Portfolio/',
   trailingSlash: true,
-  distDir: 'out',
 }
 
 module.exports = nextConfig 
